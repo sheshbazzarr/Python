043728b0ac22e92c2f7fa1621e02cdeb186f7cc3
demerwdani7452 <demerwdani@gmail.com>
2024-02-03 18:17:58 -0500

    tuple creation

---
+++ Learn/tuples.py
@@ -0,0 +1,7 @@
+#creat tuples
+animal_tuple=('yellow anaconda","Reptile",30,5,20)
+print(animal_type)
+
+#create tuple from list
+animal_tuple=tuple(["yellow anaconda","Reptile",30.5,20])
+print(animal_tuple)
