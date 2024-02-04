0dfd6f84528a13152dab1952eae757252b43aaa7
demerwdani7452 <demerwdani@gmail.com>
2024-02-03 23:59:42 -0500

    update

--- Learn/Set.py
+++ Learn/Set.py
@@ -5,3 +5,11 @@ print(eats_plants)
 eats_meat=set(["lion","tiger","bear","hawk","fox","lion"])
 
 print(eats_meat)
+
+#check membership in set
+"lion" in eats_meat
+
+#Duplicates ,since sets are unordered,they do not allow duplicates.
+eats_meat={"lion","tiger","bear","hawk","fox","lion"}
+print(eats_meat)
+
