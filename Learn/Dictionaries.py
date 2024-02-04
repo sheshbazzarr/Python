dc4b4771e6277c555c9d9de77af84f6e8129e13c
demerwdani7452 <demerwdani@gmail.com>
2024-02-04 01:22:44 -0500

    nested dictinary

--- Learn/Dictionaries.py
+++ Learn/Dictionaries.py
@@ -2,3 +2,17 @@
 our_first_dictionary={}
 our_first_dictionary['first_key']='first_value'
 print(our_first_dictionary)
+
+
+#creation of dict using dict()
+our_first_dictinary=dict(first_key='first_value',extra_key='extra_value')
+print(our_first_dictinary)
+
+#create a nested dictionary
+fynbos_familes={'erica':{'cape floristic region':670,'worldwide':4500},
+                 'protea':{'cape floristic region':330,'worldwide':1350},
+                 'restio':{'cape floristic region':273,'worldwide':1650},
+                 'citrus':{'cape floristic region':135,'worldwide':900}
+                 }
+
+print(fynbos_familes)
