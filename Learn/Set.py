d40f1f13024669a1e915486914449e8c44a3b225
demerwdani7452 <demerwdani@gmail.com>
2024-02-04 00:10:39 -0500

    update

--- Learn/Set.py
+++ Learn/Set.py
@@ -12,4 +12,19 @@ print(eats_meat)
 #Duplicates ,since sets are unordered,they do not allow duplicates.
 eats_meat={"lion","tiger","bear","hawk","fox","lion"}
 print(eats_meat)
+#adding element
+eats_plants.add('cow')
+print(eats_plants)
+eats_meat.update({'leopard','cheetah'})
+print(eats_meat)
+
+eats_plants.remove('rabbit')
+print(eats_plants)
+
+eats_plants.discard('deer')
+
+all_animals=eats_meat.union(eats_plants)
+print(all_animals)
+omnivores=eats_meat.intersection(eats_plants)
+print(ominvores)
 
