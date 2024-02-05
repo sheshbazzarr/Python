961eb42e47c8573b40b89f549b080107caaf46e5
demerwdani7452 <demerwdani@gmail.com>
2024-02-05 06:03:12 -0500

    merge sort

---
+++ Algorithms/SortingAlgo/mergeSort.py
@@ -0,0 +1,25 @@
+def merge(A,B):
+    new_list=[]
+    while len(A)>0 and len(B)>0:
+        if A[0]<B[0]:
+            new_list.append(A[0])
+            A.pop(0)
+        else:
+            new_list.append(B[0])
+            B.pop(0)
+    if len(A)==0:
+        new_list=new_list+B
+    if len(B)==0:
+        new_list=new_list +A
+
+    return new_list
+def merge_sort(items):
+    len_i=len(items)
+    if len_i==1:
+        return items
+    mid_point=int(len_i/2)
+    i1=merge_sort(items[:mid_point])
+    i2=merge_sort(items[mid_point:])
+
+    return merge(i1,i2)
+
