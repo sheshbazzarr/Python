16ac27e8eb6467c116ccb32760d9322513f60d76
demerwdani7452 <demerwdani@gmail.com>
2024-02-05 10:30:00 -0500

    power functions

---
+++ Algorithms/Recurstion/power.py
@@ -0,0 +1,7 @@
+def power(a,n):
+    if n==0:
+        return 1
+    else:
+        return a*power(a,n-1)
+print(power(2,3))
+print(power(5,0))
---
+++ Algorithms/SortingAlgo/SortAlgorithms.py
@@ -0,0 +1,16 @@
+A sorting algorithm is an algorithm that puts elements of a list in a certain logical order .
+
+built in functions such as sorted(),
+sorting in python
+
+applying sorting techniques is especially usefull
+searching algorithms are much quicker on sorted lists.
+selecting items based on a specific charactersitic such as the kth smallest or largest value
+
+finding duplicates in a list is much easier if sorted.
+
+
+an in-place
+a stable sorting algorithm refers to one leaves list elements that have equal sorting keys at the same relative postions.
+
+
