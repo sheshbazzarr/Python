ba091cf88bc858c35f785e35e439a3a919c44c28
demerwdani7452 <demerwdani@gmail.com>
2024-02-05 07:50:12 -0500

    quick and merg sort recursion method

---
+++ Algorithms/SortingAlgo/Quick_Sort.py
@@ -0,0 +1,20 @@
+def quick_sort(items,index=1):
+    len_i=len(items)
+
+    if len_i<=1:
+        return items
+    pivot=items[index]
+    small=[]
+    large=[]
+    dup=[]
+    for i in items:
+        if i <pivot:
+            small.append(i)
+        elif i>pivot:
+            large.append(i)
+        elif i==pivot:
+            dup.append(i)
+    small=quick_sort(small)
+    large=quick_sort(large)
+
+    return small+dup+large
