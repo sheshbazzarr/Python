1da1a1037c4b19c96a637fd86c9dbcf03efdda0b
demerwdani7452 <demerwdani@gmail.com>
2024-02-06 06:56:42 -0500

    counters

---
+++ Learn/Counter.py
@@ -0,0 +1,14 @@
+from collections import Counter
+my_list=['a','b','c','d','a','f','b']
+counter=Counter(my_list)
+print(counter)
+
+#accessing counter
+print(counter['a']
+print(counter.ger('b'))
+
+
+#update
+counter.update(['a','b','b','c','a'])
+print(counter)
+
