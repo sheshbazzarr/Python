c9733e8de9cf45a83020dc9fd68cbcb7d9a1c0e1
sheshbzzarr <demerwdani@gmail.com>
2025-07-01 13:50:03 -0500

    Add graph algo(bfs implementation )

---
+++ Graph/bfs/queue.py
@@ -0,0 +1,34 @@
+from node import Node 
+class Queue:
+    __head=None 
+    __tail=None 
+    __count=0
+
+    def add(self,element):
+        if self.__head ==None:
+            self.__head=Node(element,None,None)
+            
+            self.__tail=self.__head 
+        else:
+            new_node=Node(element,None,None)
+            self.__tail.next=new_node 
+            new_node.prev=self.__tail 
+            self.__tail=new_node 
+            self.__count+=1
+        def poll(self):
+            p=self.__head 
+            self.__head=self.__head.next 
+            p.next=None 
+            p.prev=None 
+            self.__count-=1
+            return p.data 
+        def peek(self):
+            return self.__head.data 
+        def size(self):
+            return self.__count 
+        def isempty(self):
+            if self.__count==0: 
+                return True 
+            else:
+                return False 
+            
