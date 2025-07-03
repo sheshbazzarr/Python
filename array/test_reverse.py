2869117bd6e23da5b6f45073b975e63dad293b28
sheshbzzarr <demerwdani@gmail.com>
2025-07-03 04:06:12 -0500

    test_reverse.py

--- array/test_reverse.py
+++ array/test_reverse.py
@@ -2,11 +2,12 @@ import sys
 def reverse(array):
     length=len(array)
     middle=int(length/2)
-    for i in range(0,middle):
+    for i in range(middle):
         temp=array[i]
         array[i]=array[length-i-1]
+        array[length]=temp 
+    return array 
 
-        array[length-i-1]=temp 
 def main():
     scores=[50,60,70,80,90]
     reverse(scores)
